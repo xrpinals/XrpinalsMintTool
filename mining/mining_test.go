3ac2936a952e8dd812b0ebdec3db22fa4fa01a93
LuisNajmark <xrpinals-protocol@proton.me>
2023-12-19 13:36:34 +0800

    add assets for xps

---
+++ mining/mining_test.go
@@ -0,0 +1,71 @@
+package mining
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/Xrpinals-Protocol/XrpinalsMintTool/bitcoin"
+	"math/big"
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestMining(t *testing.T) {
+	var nBits uint32 = 0x1e00ffff
+	var wg sync.WaitGroup
+
+	var isStop atomic.Bool
+	isStop.Store(false)
+
+	var numCPU = runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPU)
+	if numCPU > 1 {
+		MinerNum = numCPU - 1
+	}
+
+	start := time.Now()
+
+	for i := 0; i < MinerNum; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, nBits uint32, i int, step int, isStop *atomic.Bool) {
+			defer wg.Done()
+
+			var nonce uint64 = 0
+			target := bitcoin.NBits2Target(nBits)
+
+			for {
+				payloadBytes := make([]byte, 80)
+				rand.Read(payloadBytes)
+
+				s256 := sha256.New()
+				_, _ = s256.Write(payloadBytes)
+				hashBytes := s256.Sum(nil)
+				result := new(big.Int).SetBytes(hashBytes)
+
+				if result.Cmp(target) < 0 {
+					if isStop.CompareAndSwap(false, true) {
+						return
+					} else {
+						return
+					}
+				} else {
+					if isStop.Load() {
+						return
+					}
+				}
+
+				// next nonce
+				nonce = nonce + uint64(step)
+			}
+
+		}(&wg, nBits, i, MinerNum, &isStop)
+	}
+	wg.Wait()
+
+	duration := time.Since(start)
+
+	fmt.Println(duration)
+}
